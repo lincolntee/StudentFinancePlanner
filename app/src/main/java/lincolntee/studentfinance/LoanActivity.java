ed2f5ca515c0f6951a2a044809982f912af49d68
lincolntee <teebh-wa14@student.tarc.edu.my>
2018-01-15 22:54:54 +0800

    loan activity

--- app/src/main/java/lincolntee/studentfinance/LoanActivity.java
+++ app/src/main/java/lincolntee/studentfinance/LoanActivity.java
@@ -5,15 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoanActivity extends MainActivity {
 
+    TextView loanAmount, loanDuration, paymentDueDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan);
+
         Button buttonBack = (Button) findViewById((R.id.buttonBack_LA)); //return to main menu
         Button buttonUpdateLoanInformation = (Button) findViewById(R.id.buttonUpdateLoanInformation);
+
+        loanAmount = (TextView)findViewById(R.id.textViewLoanAmount);
+        loanDuration = (TextView)findViewById(R.id.textViewLoanDuration);
+        paymentDueDate = (TextView)findViewById(R.id.textViewPaymentDueDate);
+
+        loanAmount.setText("Amount : " + getIntent().getStringExtra("LOANAMOUNT"));
+        loanDuration.setText("Duration : " + getIntent().getStringExtra("LOANDURATION"));
+        paymentDueDate.setText("Due Date : " + getIntent().getStringExtra("PAYMENTDUEDATE"));
 
         buttonBack.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view)
