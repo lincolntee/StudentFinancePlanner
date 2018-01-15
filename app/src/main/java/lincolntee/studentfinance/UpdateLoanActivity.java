1c6435290a4613e587a7657bd4fc6213b2b7215e
lincolntee <teebh-wa14@student.tarc.edu.my>
2018-01-15 22:56:05 +0800

    loan activity

---
+++ app/src/main/java/lincolntee/studentfinance/UpdateLoanActivity.java
@@ -0,0 +1,50 @@
+package lincolntee.studentfinance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UpdateLoanActivity extends AppCompatActivity {
+
+    EditText editTextLoanAmount;
+    EditText editTextLoanDuration;
+    EditText editTextPaymentDueDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_loan);
+        editTextLoanAmount = (EditText)findViewById(R.id.editTextLoanAmount);
+        editTextLoanDuration = (EditText)findViewById(R.id.editTextLoanDuration);
+        editTextPaymentDueDate = (EditText)findViewById(R.id.editTextPaymentDueDate);
+        Button buttonUpdateLoanBack = (Button) findViewById(R.id.buttonUpdateLoanBack_LA);
+        Button buttonUpdateLoan = (Button) findViewById(R.id.buttonUpdateLoan);
+        buttonUpdateLoanBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view)
+            {
+                // Return to main activity
+                Intent intent = new Intent(UpdateLoanActivity.this, LoanActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonUpdateLoan.setOnClickListener(new View.OnClickListener()
+        {
+          @Override
+            public void onClick(View view)
+          {
+              String loanAmount = editTextLoanAmount.getText().toString();
+              String loanDuration = editTextLoanDuration.getText().toString();
+              String paymentDueDate = editTextPaymentDueDate.getText().toString();
+              Intent intent = new Intent(UpdateLoanActivity.this, LoanActivity.class);
+              intent.putExtra("LOANAMOUNT",loanAmount);
+              intent.putExtra("LOANDURATION", loanDuration);
+              intent.putExtra("PAYMENTDUEDATE",paymentDueDate);
+              startActivity(intent);
+          }
+
+        });
+    }
+}
