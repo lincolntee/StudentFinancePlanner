cf567215073f86ec39e95a094683c286f071f570
Lincoln <teebh-wa14@student.tarc.edu.my>
2018-01-17 06:42:26 +0800

    d

--- app/src/main/java/lincolntee/studentfinance/UpdateProfileActivity.java
+++ app/src/main/java/lincolntee/studentfinance/UpdateProfileActivity.java
@@ -1,11 +1,19 @@
 package lincolntee.studentfinance;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 
@@ -15,17 +23,52 @@ public class UpdateProfileActivity extends AppCompatActivity {
     EditText editTextIcNumber;
     EditText editTextYearIntake;
     Button buttonUpdate;
+    Button buttonGetLocation;
+    TextView textViewCoordinate;
+
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
 
-        editTextName = (EditText) findViewById(R.id.editTextName);
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextStudentId = (EditText) findViewById(R.id.editTextStudentID);
-        editTextIcNumber = (EditText) findViewById(R.id.editTextIcNumber);
-        editTextYearIntake = (EditText) findViewById(R.id.editTextYearIntake);
-        buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextStudentId = findViewById(R.id.editTextStudentID);
+        editTextIcNumber = findViewById(R.id.editTextIcNumber);
+        editTextYearIntake = findViewById(R.id.editTextYearIntake);
+        buttonUpdate = findViewById(R.id.buttonUpdate);
+        buttonGetLocation = findViewById(R.id.buttonGetLocation);
+        textViewCoordinate = findViewById(R.id.textViewCoordinate);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+
         buttonUpdate.setOnClickListener(new View.OnClickListener()
         {
             @Override
